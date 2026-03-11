f88a1cec56256d409f714d37dc6ac0bff63ef296
MananUtsav07 <71372287+MananUtsav07@users.noreply.github.com>
2026-03-11 11:32:28 +0530

    vercel

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cors from 'cors'
 import express, { type Request } from 'express'
-import helmet from 'helmet'
+import * as helmet from 'helmet'
 import morgan from 'morgan'
 
 import { env } from './config/env.js'
@@ -45,7 +45,7 @@ export function createApp() {
       credentials: true,
     }),
   )
-  app.use(helmet())
+  app.use(helmet.default())
   app.use(express.json({ limit: '1mb' }))
   app.use(express.urlencoded({ extended: true }))
   app.use(morgan(':method :url :status :response-time ms reqId=:requestId'))
